63675f3272b59216e73aeca9446fad7fe036f887
Kien Ngoc Nguyen <tgsoon2002@gmail.com>
2016-12-06 15:12:42 -0800

    Add conversationManager, conversasion, emoji and file class

--- FullertonChatApp/FullertonChatApp/Conversation.cpp
+++ FullertonChatApp/FullertonChatApp/Conversation.cpp
@@ -5,10 +5,15 @@ using namespace std;
 class Conversation
 {
 public:
-	vector<User> userList;
+	list<User> groupMembers;
+	list<Message> messageList;
 	Conversation();
 	~Conversation();
-
+	void CreateMessage(string msgText, EmojiObject emo, FileObject file, int userID	) {
+		Message tempMsg = Message(msgText, emo, file, userID);
+		messageList.push_front(tempMsg);
+ 
+	}
 private:
 
 };
@@ -16,8 +21,74 @@ private:
 Conversation::Conversation()
 {
 	
+	
 }
 
 Conversation::~Conversation()
+{
+}
+
+
+class Message
+{
+public:
+	string text;
+	EmojiObject emoji;
+	FileObject file;
+	int userID;
+	Message();
+	Message(string txt, EmojiObject emo , FileObject fil, int uID) {
+		text = txt;
+		emoji = emo;
+		file = fil;
+		userID = uID;
+	};
+	~Message();
+
+private:
+
+};
+
+Message::Message()
+{
+}
+
+Message::~Message()
+{
+}
+
+class FileObject
+{
+public:
+	FileObject();
+	~FileObject();
+
+private:
+
+};
+
+FileObject::FileObject()
+{
+}
+
+FileObject::~FileObject()
+{
+}
+
+class EmojiObject
+{
+public:
+	EmojiObject();
+	~EmojiObject();
+
+private:
+
+};
+
+EmojiObject::EmojiObject()
+{
+}
+
+EmojiObject::~EmojiObject()
 {
 }
